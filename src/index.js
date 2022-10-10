f4a053ec99f1b81f5703f7844e16e8da76e03bc0
jocad <ochlillen@gmail.com>
2022-10-10 00:26:05 -0400

    challenge done

---
+++ src/index.js
@@ -0,0 +1,17 @@
+const shareBtn = document.querySelector('.shareIcon-container');
+const shareIcon = document.querySelector('#share-icon');
+const shareOpt = document.querySelector('.share-top');
+
+shareBtn.addEventListener('click', ()=>{
+    shareOpt.classList.toggle('inactive')
+    // shareBtn.classList.toggle('shareIcon-container-clicked');
+    // shareIcon.classList.toggle('share-icon-toggle')
+    const shareIconColor = shareIcon.getAttribute('fill');
+    if(shareIconColor === '#6E8098'){
+        shareIcon.setAttribute('fill', 'var(--Light-Grayish-Blue)');
+        shareBtn.setAttribute('style', 'background-color: var(--Desaturated-Dark-Blue)');
+    } else {
+        shareIcon.setAttribute('fill', '#6E8098');
+        shareBtn.setAttribute('style', 'background-color: var(--Light-Grayish-Blue)');
+    }
+})
